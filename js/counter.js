01fd1553011964e16004bb09476f7f879c8c9595
CJ1138 <chrisjohnson1138@gmail.com>
2022-09-23 12:01:11 +0100

    Finished adding API authentication

--- js/counter.js
+++ js/counter.js
@@ -1,5 +1,7 @@
+const apikey = config.API_KEY;
+
 function counter(){
-    fetch('https://crc-api-gateway-5fntyvv6.nw.gateway.dev/visits', {
+    fetch('https://crc-api-gateway-5fntyvv6.nw.gateway.dev/visits?key=' + apikey, {
     method: 'POST'})
     .then((response) => response.json())
     .then((data) => {
