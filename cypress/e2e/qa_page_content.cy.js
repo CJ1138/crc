6bf4d3d779c29fdda43bd453270dcac5ac1911b3
CJ1138 <chrisjohnson1138@gmail.com>
2022-10-13 11:11:24 +0100

    Adding to Cypress tests.

--- cypress/e2e/qa_page_content.cy.js
+++ cypress/e2e/qa_page_content.cy.js
@@ -1,36 +1,46 @@
 /// <reference types="Cypress" />
 
-describe('Page Content Loads Correctly', () => {
+import rgbHex from 'rgb-hex';
+
+describe('Page Content Loads', () => {
     it('Finds the words "Professional Experience"', ()=>{
         cy.visit(Cypress.env('RESUME_PAGE'))
-
         cy.contains("Professional Experience")
     })
 
-    it('Checks the blog link', ()=>{
+    it('Checks for Visitor Counter', {defaultCommandTimeout: 10000}, ()=>{
         cy.visit(Cypress.env('RESUME_PAGE'))
-
-        cy.contains('Technical Blog')//.click()
-
-        //cy.contains('Recent Posts')
-    })
-
-    it('Checks for Visitor Counter', ()=>{
-        cy.visit(Cypress.env('RESUME_PAGE'))
-
+        //Checks vistor counter text displays
         cy.contains('You are visitor number')
 
+        //Checks that visitor count is integer
         cy.get('[id=count]')
         .invoke('text')
         .should('match', /^[0-9]*$/)
     })
 
-    it('check all links to sites', () => {
-
+    it('Checks outbound links', () => {
+        //Ignores weird 999 error from LinkedIn
+        Cypress.on('fail', (error, runnable) => {
+            if (!error.message.includes('999')) {
+                throw error
+            }
+        })
+        
         cy.visit(Cypress.env('RESUME_PAGE'))
+        //Loops through all links except email
         cy.get("a:not([href*='mailto:'])").each(page => {
-          cy.request(page.prop('href'))
+            cy.request(page.prop('href'));
         })
       
       });
+
+    it('Checks background colour', () => {
+      cy.get('[id=col1]')
+      .invoke('css', 'background-color')
+      .then((bgcolor) => {
+        expect(rgbHex(bgcolor)).to.eq('536979')
+      })
+    })
+
 })
